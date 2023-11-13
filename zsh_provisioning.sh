d528d0b639f2eb0f0955ad6cc506aec85137c468
vmath3us <vmatheusborges@gmail.com>
2023-11-13 05:03:19 -0300

    [FEATURE] provisioning profiles

--- zsh_provisioning.sh
+++ zsh_provisioning.sh
@@ -1,63 +1,107 @@
 #!/bin/zsh
-apk add --update \
-        ansible \
-        iproute2 \
-        htop \
-        helm \
-        tar \
-        zstd \
-        gzip \
-        lz4 \
-        lzo \
-        xz \
-        libarchive-tools \
-        findmnt \
-        openssh \
-        gpg \
-        gpg-agent \
-        icu-data-full \
-        jq \
-        lynx \
-        zsh \
-        bat \
-        tree \
-        py3-pip \
-        bash \
-        git \
-        lua \
-        nodejs \
-        npm \
-        lazygit \
-        bottom \
-        python3 \
-        go \
-        neovim \
-        ripgrep \
-        alpine-sdk \
-        github-cli \
-        git-lfs \
-        podman \
-        docker \
-        zsh-vcs \
-        fd \
-        diffutils \
-        findutils \
-        ranger \
-        pv \
-        glab &&
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim &&
-git clone https://github.com/AstroNvim/user_example ~/.config/nvim/lua/user &&
+function minimal_userspace()
+{
+    apk add --update \
+            git \
+            bash \
+            neovim \
+            htop \
+            tar \
+            zstd \
+            gzip \
+            lz4 \
+            lzo \
+            xz \
+            libarchive-tools \
+            openssh \
+            gpg \
+            zsh-vcs \
+            ripgrep \
+            icu-data-full \
+            bat \
+            tree \
+            fd
+    MINIMAL_PROVISIONED="true"
+    return
+}
+function large_userspace()
+{
+    apk add --update \
+            ansible \
+            iproute2 \
+            helm \
+            findmnt \
+            jq \
+            lynx \
+            py3-pip \
+            github-cli \
+            git-lfs \
+            podman \
+            docker \
+            zsh-vcs \
+            diffutils \
+            findutils \
+            ranger \
+            pv \
+            glab
 curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl &&
-chmod +x /usr/local/bin/kubectl &&
-curl -L https://gitlab.com/vmath3us/devops-userspace/-/raw/main/zshrc -o /root/zshrc &&
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
-/root/.fzf/install --completion --key-bindings --update-rc &&
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &&
-cp /root/zshrc ~/.zshrc &&
-exit 0 || exit 1
+chmod +x /usr/local/bin/kubectl
+    return
+}
+function editor()
+{
+    if [ -z $MINIMAL_PROVISIONED ] ; then
+        apk add bash git neovim ripgrep
+        MINIMAL_PROVISIONED="downstream"
+    fi
+    apk add lua \
+            nodejs \
+            npm \
+            lazygit \
+            bottom \
+            python3 \
+            go \
+            alpine-sdk --update &&
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim &&
+git clone https://github.com/AstroNvim/user_example ~/.config/nvim/lua/user
+    return
+}
+function shell()
+{
+    if [ -z $MINIMAL_PROVISIONED ] ; then
+        apk add bash git neovim ripgrep
+        MINIMAL_PROVISIONED="downstream"
+    fi
+    curl -L https://gitlab.com/vmath3us/devops-userspace/-/raw/main/zshrc -o /root/zshrc &&
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
+    /root/.fzf/install --completion --key-bindings --update-rc &&
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &&
+    cp /root/zshrc ~/.zshrc
+    return
+}
 #--[no-]key-bindings  Enable/disable key bindings (CTRL-T, CTRL-R, ALT-C)
 #    --[no-]completion    Enable/disable fuzzy completion (bash & zsh)
 #    --[no-]update-rc     Whether or not to update shell configuration files
+function main()
+{
+    if [ $PROFILE = "full" ] ; then
+        minimal_userspace &&
+        large_userspace &&
+        editor &&
+        shell
+    elif [ $PROFILE = "minimal" ] ; then
+        minimal_userspace &&
+        editor &&
+        shell
+    elif [ $PROFILE = "editor" ] ; then
+        editor &&
+        shell
+    elif [ $PROFILE = "shell" ] ; then
+        shell
+    fi
+}
+if [ -z $PROFILE ] ; then PROFILE="full" ; fi
+main
