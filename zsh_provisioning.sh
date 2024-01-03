2041b58a81f5f298ac245656ad36ccfe0d4e706f
vmath3us <vmatheusborges@gmail.com>
2024-01-03 09:05:13 -0300

    [ADD] comentario com sed para troca de usuário

--- zsh_provisioning.sh
+++ zsh_provisioning.sh
@@ -81,6 +81,8 @@ function shell()
     ~/.fzf/install --completion --key-bindings --update-rc &&
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &&
     cp ~/zshrc ~/.zshrc
+    # sed -i "s|/root|$HOME|g" .zshrc
+    # exec zsh
     return
 }
 #--[no-]key-bindings  Enable/disable key bindings (CTRL-T, CTRL-R, ALT-C)
