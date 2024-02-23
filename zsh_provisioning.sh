c79ea00a41c45d310971966b258287d1fee6f08b
vmath3us <vmatheusborges@gmail.com>
2024-02-23 17:59:44 -0300

    [ADD] package: fancy tmux https://zellij.dev

--- zsh_provisioning.sh
+++ zsh_provisioning.sh
@@ -57,6 +57,7 @@ function large_userspace()
             findutils \
             ranger \
             pv \
+            zellij \
             glab
 curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl &&
 chmod +x /usr/local/bin/kubectl
