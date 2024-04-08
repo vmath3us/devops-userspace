0ac8b19fadb641e15d2294978bed4417e132c904
vmath3us <vmatheusborges@gmail.com>
2024-04-08 12:04:41 -0300

    [ADD] script para setup bare-metal (only shell, ubuntu)

---
+++ base-metal-ubuntu-zsh-shell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ root = "root" ] ; then
+	mkdir -pv /var/lib/root-save/{work,upper} &&
+	if ! mountpoint /root ; then
+	mount -t overlay overlay -o lowerdir=/root,upperdir=/var/lib/root-save/upper/,workdir=/var/lib/root-save/work/ /root
+	fi &&
+	if [ ! -e /root/.zshrc ] ; then
+	    echo "instalando dependencias em 10 segundos, ctrl-c para cancelar"
+	    echo "comando: apt install -y git bash curl zsh"
+	    for i in {10..1} ; do
+	        echo -ne "$i restantes\r"
+	        sleep 1
+	    done &&
+	    apt update &&
+	    apt install -y git bash zsh curl ; curl -fsL https://gitlab.com/vmath3us/devops-userspace/-/raw/main/zsh_provisioning.sh | MINIMAL_PROVISIONED=1 PROFILE=shell zsh || exit 1
+	fi && echo "execute: 
+                exec zsh
+                "
+else
+    echo "execute como root, saindo com erro"
+    exit 1
+fi
