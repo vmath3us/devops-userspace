72110877892d519cf2af3a0274241e1813aa0155
vmath3us <vmatheusborges@gmail.com>
2024-11-29 23:59:50 -0300

    [ADD] systemd + lxqt build

---
+++ nested-workstation/lxqt/privileged-dind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#podman build . -t podman-workstation-lxqt-systemd --layers=true --format=docker
+docker build . -t docker-workstation-lxqt-systemd --progress=plain
+export PASS=$(uuidgen| sed 's/-//g')
+docker run \
+            -dt \
+            --env PASS="${PASS}" \
+            -p 127.0.0.1:15000:15000 \
+            --shm-size 1G \
+            --privileged \
+            -v home-docker-workstation-lxqt-systemd:/home/ubuntu:Z \
+            -v docker-workstation-persistence-lxqt-systemd:/var/lib/docker:Z \
+            --name docker-workstation-lxqt-systemd \
+            docker-workstation-lxqt-systemd:latest
+docker logs -f docker-workstation-lxqt-systemd
