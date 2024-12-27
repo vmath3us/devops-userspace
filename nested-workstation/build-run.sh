d8b01a2087154facf301417ce4d642ab15531f07
vmath3us <vmatheusborges@gmail.com>
2024-12-27 18:28:16 -0300

    [ADD] xpra setup: espaço de trabalho em docker
        -> https://xpra.org
    
        xhost, xfce, lxqt ou plasma (via ENVIRONMENT, padrão: xhost)
    
        Firefox , VsCodium, Chrome, VsCode (se INCLUDE_EXTRAS=true; padrão: false)
    
        Terminator e Tmux pré-configurados
    
        Docker em modos nested (DinD), ou via host socket (binários diretamente da imagem docker:latest)
    
        Kind funcionará também em modo DinD (testado)
        -> https://kind.sigs.k8s.io/
    
        É exposto somente em 127.0.0.1:15000, e não localhost, por padrão
    
        Relacionado:
        -> github.com  Xpra-org xpra issues 4383 #issuecomment-2564289439

---
+++ nested-workstation/build-run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#############---build
+set -e
+docker pull docker:latest
+docker pull ubuntu:24.04
+
+DOCKER_IMAGE_DIGEST=$(docker image inspect docker:latest --format='{{index .RepoDigests 0}}')
+UBUNTU_IMAGE_DIGEST=$(docker image inspect ubuntu:24.04 --format='{{index .RepoDigests 0}}')
+
+unset -- INCLUDE_EXTRAS ENVIRONMENT PASS DOCKER_MODE
+
+source ./.env-xpra
+
+docker build . \
+            -t docker-workstation-includeextras-"${INCLUDE_EXTRAS}"-interfacemode-"${ENVIRONMENT}" \
+            --progress=plain \
+            --build-arg UBUNTU_IMAGE_DIGEST="${UBUNTU_IMAGE_DIGEST}" \
+            --build-arg DOCKER_IMAGE_DIGEST="${DOCKER_IMAGE_DIGEST}" \
+            --build-arg INCLUDE_EXTRAS="${INCLUDE_EXTRAS}" \
+            --build-arg ENVIRONMENT="${ENVIRONMENT}"
+
+
+#############---run
+if [[ $DOCKER_MODE = "nested" ]] ; then
+    export DOCKER_MODE_STRING=" -p 127.0.0.1:15000:15000 \
+                                --privileged \
+                                -v docker-workstation-persistence-"${ENVIRONMENT}":/var/lib/docker:Z"
+elif [[ $DOCKER_MODE = "socket" ]] ; then
+    export DOCKER_MODE_STRING=" --net host \
+                                --privileged \
+                                -v /var/run/docker.sock:/docker.sock:Z"
+else
+    exit 1
+fi
+eval "docker run \
+            -d \
+            --shm-size=1G \
+            --env PASS="${PASS}" \
+            --env ENVIRONMENT="${ENVIRONMENT}" \
+            --env XDG_CURRENT_DESKTOP="${ENVIRONMENT}" \
+            --env XDG_SESSION_DESKTOP="${ENVIRONMENT}" \
+            -v home-docker-workstation-docker-"${ENVIRONMENT}":/home/ubuntu:Z \
+            "${DOCKER_MODE_STRING}" \
+            --name docker-workstation-"${ENVIRONMENT}" \
+            --hostname docker-workstation-"${ENVIRONMENT}" \
+            docker-workstation-includeextras-"${INCLUDE_EXTRAS}"-interfacemode-"${ENVIRONMENT}"
+"
+docker logs -f docker-workstation-"${ENVIRONMENT}"
