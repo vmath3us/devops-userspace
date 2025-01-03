3996ad6865d0c56eb11966018bb94b7cb7a35ee6
vmath3us <vmatheusborges@gmail.com>
2025-01-03 19:40:01 -0300

    [CHG] libvirt container: systemd

--- libvirt-ubuntu-container.sh
+++ libvirt-ubuntu-container.sh
@@ -3,9 +3,11 @@ set -e
 ##### flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo --user
 ##### flatpak install --user org.virt_manager.virt-manager
 if [ -z ${CONTAINER_ENGINE} ] ; then CONTAINER_ENGINE='docker' ; fi
-mkdir -pv /var/run/libvirt
+rm -rv /run/libvirt /run/libvirt-container
+mkdir /run/libvirt-container
+ln -sf /run/libvirt-container/libvirt /run/libvirt
 ${CONTAINER_ENGINE} run \
-    -d \
+    -dt \
     --name libvirt \
     --net host \
     --cgroupns host \
@@ -22,15 +24,34 @@ ${CONTAINER_ENGINE} run \
     -v libvirt-state-save:/etc/libvirt:Z \
     -v libvirt-cache-save:/var/cache:Z \
     -v libvirt-root-save:/root:Z \
-    -v /var/run/libvirt:/var/run/libvirt:Z \
+    -v /run/libvirt-container:/run:Z \
     ubuntu:24.04 bash -xc "
                         export DEBIAN_FRONTEND=noninteractive
                         apt-get update
                         apt-get install -y \
                             libvirt-daemon-system \
                             qemu-kvm
-                        setsid --fork /usr/sbin/virtlogd >/dev/null 2>/dev/null
-                        sleep 10
-                        setsid --fork /usr/sbin/virtlockd >/dev/null 2>/dev/null
-                        /usr/sbin/libvirtd
-                        sleep infinity"
+                            systemd
+                        UNIT_TARGETS_TO_MASK='
+		                        /usr/lib/systemd/system/*.mount
+		                        /usr/lib/systemd/system/console-getty.service
+		                        /usr/lib/systemd/system/getty@.service
+		                        /usr/lib/systemd/system/systemd-machine-id-commit.service
+		                        /usr/lib/systemd/system/systemd-binfmt.service
+		                        /usr/lib/systemd/system/systemd-tmpfiles*
+		                        /usr/lib/systemd/system/systemd-udevd.service
+		                        /usr/lib/systemd/system/systemd-udev-trigger.service
+		                        /usr/lib/systemd/systemd-resolved
+		                        /usr/lib/systemd/system/modprobe*
+		                        /usr/lib/systemd/system/dpkg*
+		                        /usr/lib/systemd/system/apt*
+		                        /usr/lib/systemd/system/systemd-update-utmp*
+	                    '
+                        for unit in \$(find \${UNIT_TARGETS_TO_MASK} 2> /dev/null); do
+                            systemctl mask \"\$(basename \"\${unit}\")\" || :
+                        done
+                        for socket_unit in 'libvirtd.socket' 'libvirtd-admin.socket' 'libvirtd-ro.socket' ; do
+                            sed -i 's/SocketMode.*/SocketMode=0777/g' /usr/lib/systemd/system/\$socket_unit
+                        done
+                        echo -e 'ForwardToConsole=yes\nTTYPath=/dev/console' >> /etc/systemd/journald.conf
+                        /usr/lib/systemd/systemd"
